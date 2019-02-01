dc7987b7fb2ce982b1df537d871b8e89ad388b0b
Wouter Schievink <meatcomputer@protonmail.com>
2019-02-01 00:54:20 -0500

    got flask app running

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import glob, random
 app = Flask(__name__)
+cities = glob.glob("./static/citymaps/*/") 
+image_pos=0
+ciry_dir=random.choice(cities)
+@app.route("/",methods = ['POST', 'GET'])
+def mapframe():
+    if request.method == 'POST':
+        if request.form['new_map_button']:
+            print("did it")
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
 
-@app.route("/")
-def hello():
-    return "Map Frame"
+
+@app.route("/city")
+def city():
+    city_dir = random.choice(cities) 
+    images = ["1.jpg","2.jpg","3.jpg","6.jpg"]
+    image_dir = city_dir+images[0]
+    return render_template("city.html", image_dir=image_dir)
+
+@app.route("/new_city")
+def new_city():
+    global city_dir
+    city_dir = random.choice(cities) 
+    print(city_dir)
+    return city_dir
+
+@app.route("/city_url")
+def city_url():
+    global image_pos
+    global city_dir
+    images = ["1.jpg","2.jpg","3.jpg","6.jpg"]
+    image_dir = city_dir+images[image_pos]
+    if image_pos < 3:
+        image_pos += 1  
+    else:
+        image_pos = 0
+    return image_dir
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- slideshow.py
+++ slideshow.py
@@ -12,55 +12,53 @@ import os,pdb
 import glob
 import pyglet
 
-cities = glob.glob("./citymaps/*/")
-city = random.choice(cities)
-images = ["1.jpg","2.jpg","3.jpg","6.jpg"]
-image_pos = 0
+class SlideShow:
+    def __init__(self):
+        self.cities = glob.glob("./citymaps/*/")
+        self.city = random.choice(self.cities)
+        self.images = ["1.jpg","2.jpg","3.jpg","6.jpg"]
+        self.image_pos = 0
+        self.window = pyglet.window.Window()
+        self.img = pyglet.image.load(self.city+self.images[self.image_pos])
+        self.sprite = pyglet.sprite.Sprite(self.img)
+        
+    def update_city(self, dt):
+        self.city = random.choice(self.cities)
+
+    def update_image(self, dt):
+        print("updating")
+        self.img = pyglet.image.load(self.city+self.images[self.image_pos])
+        self.sprite.image = self.img
+        self.sprite.scale = self.get_scale(self.window, self.img)
+        self.sprite.x = 0
+        self.sprite.y = 0
+        if self.image_pos > 2:
+            self.image_pos = 0
+        else: 
+            self.image_pos += 1
+        
+        self.window.clear()
+        self.sprite.draw()
+
+
+    def get_scale(self, window, image):
+        if image.width > image.height:
+            scale = float(window.width) / image.width
+        else:
+            scale = float(window.height) / image.height
+        return scale
 
 
 
 
-def update_city(dt):
-    global city
-    city = random.choice(cities)
+  # @self.window.event
+  # def on_draw(self):
+  #     self.sprite.draw()
 
-def update_image(dt):
-    global image_pos
-    img = pyglet.image.load(city+images[image_pos])
-    sprite.image = img
-    sprite.scale = get_scale(window, img)
-    sprite.x = 0
-    sprite.y = 0
-    if image_pos > 2:
-        image_pos = 0
-    else: 
-        image_pos += 1
-    window.clear()
-
-
-def get_scale(window, image):
-    if image.width > image.height:
-        scale = float(window.width) / image.width
-    else:
-        scale = float(window.height) / image.height
-    return scale
-
-
-window = pyglet.window.Window(fullscreen=True)
-
-
-@window.event
-def on_draw():
-    sprite.draw()
-
-
-if __name__ == '__main__':
-
-    image_pos = 0
-    img = pyglet.image.load(city+images[image_pos])
-    image_pos+=1
-    sprite = pyglet.sprite.Sprite(img)
-    sprite.scale = get_scale(window, img)
-    pyglet.clock.schedule_interval(update_image, 10)
-    pyglet.clock.schedule_interval(update_city, 300)
-    pyglet.app.run()
+    def run(self):
+        self.sprite.scale = self.get_scale( self.window, self.img)
+        print("got here")
+        self.sprite.draw()
+        pyglet.clock.schedule_interval(self.update_image, 2)
+        pyglet.clock.schedule_interval(self.update_city, 300)
+        pyglet.app.run()
