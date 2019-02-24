3a1df67833b5b93eee9c4d12dcf6c9a649664a70
Wouter Schievink <meatcomputer@protonmail.com>
2019-02-23 22:41:29 -0500

    leaflet working

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, render_template, jsonify
 import glob, random, json, requests, pdb
 app = Flask(__name__)
-cities = glob.glob("./static/data_maps/*/") 
-image_pos=0
-city_dir=random.choice(cities)
 
-def get_city_data():
-    json_file = open(city_dir+"city_data.json").read()
-    data = json.loads(json_file)
-    return data
+def get_random_city():
+    json_cities = open('./citieslatlong.json').read()
+    dict_cities = json.loads(json_cities)
+    chosen_city = random.choice(dict_cities)
+    return chosen_city
 
-city_info=get_city_data()
+city_info=get_random_city()
 
 def k_to_f(k):
     return ((k - 273) * 1.8 ) + 32
@@ -28,16 +26,11 @@ def mapframe():
 def city():
     return render_template("interactive_city.html")
 
-@app.route("/offline_city")
-def offline_city():
-    return render_template("city.html")
-
 @app.route("/new_city")
 def new_city():
     global city_dir
     global city_info
-    city_dir = random.choice(cities) 
-    city_info = get_city_data()
+    city_info = get_random_city()
     weather_response = json.loads(get_weather())
     weather = weather_response["weather"][0]["main"]
     temperature = str(int(k_to_f(weather_response["main"]["temp"])))
@@ -48,26 +41,12 @@ def new_city():
 @app.route("/city_data")
 def city_data():
     global city_info
-    city_info=get_city_data()
     weather_response = json.loads(get_weather())
     weather = weather_response["weather"][0]["main"]
     temperature = str(int(k_to_f(weather_response["main"]["temp"])))
     city_info["weather"] = weather
     city_info["temperature"] = temperature
     return jsonify(city_info)
-
-@app.route("/city_url")
-def city_url():
-    global image_pos
-    global city_dir
-    global city_info
-    images = ["1.jpg","2.jpg","3.jpg","6.jpg"]
-    image_dir = city_dir+images[image_pos]
-    if image_pos < 3:
-        image_pos += 1  
-    else:
-        image_pos = 0
-    return image_dir
 
 @app.route("/get_weather")
 def get_weather():
