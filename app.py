e5c57c818c1c7c914a3b69148f69b4928a8453bc
Wouter Schievink <meatcomputer@protonmail.com>
2019-03-06 01:13:33 -0500

    added

--- app.py
+++ app.py
@@ -67,6 +67,9 @@ def get_random_city_from_database():
 def mapframe():
     if request.method == 'POST':
         if request.form['new_map_button']:
+            new_city()
+            return render_template('index.html')
+        if request.form['add_city_button']:
             user_selected_cities = request.form['cities'].split(',')
             add_city(user_selected_cities)
             new_city()
